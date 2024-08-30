fcf17c2eb71ac017fc45d358b2df6a112c3b1bb4
Kai Waldrant <kai@data-intuitive.com>
2024-08-30 20:45:47 +0200

    update process_dataset to include random subsample

--- src/process_dataset/script.py
+++ src/process_dataset/script.py
@@ -28,25 +28,33 @@ print(">> Load Data", flush=True)
 adata = ad.read_h5ad(par["input"])
 
 
+
+
 # limit to 1 batch with big cellxgene datasets
 if adata.n_obs > par["n_obs_limit"]:
+    print(">> Subsampling the observations", flush=True)
     print(f">> Setting seed to {par['seed']}")
     random.seed(par["seed"])
-    batch_counts = adata.obs.groupby('batch').size()
-    filtered_batches = batch_counts[batch_counts <= par["n_obs_limit"]]
-    sorted_filtered_batches = filtered_batches.sort_values(ascending=False)
-    selected_batch = sorted_filtered_batches.index[0]
-    adata = adata[adata.obs["batch"]==selected_batch,:]
+    if "batch" not in adata.obs:
+        obs_filt = np.ones(dtype=np.bool_, shape=adata.n_obs)
+        obs_index = np.random.choice(np.where(obs_filt)[0], par["n_obs_limit"], replace=False)
+        adata_output = adata[obs_index].copy()
+    else:
+        batch_counts = adata.obs.groupby('batch').size()
+        filtered_batches = batch_counts[batch_counts <= par["n_obs_limit"]]
+        sorted_filtered_batches = filtered_batches.sort_values(ascending=False)
+        selected_batch = sorted_filtered_batches.index[0]
+        adata_output = adata[adata.obs["batch"]==selected_batch,:].copy()
         
 
 
 # remove all layers except for counts
-for key in list(adata.layers.keys()):
+for key in list(adata_output.layers.keys()):
     if key != "counts":
-        del adata.layers[key]
+        del adata_output.layers[key]
 
 # round counts and convert to int
-counts = np.array(adata.layers["counts"]).round().astype(int)
+counts = np.array(adata_output.layers["counts"]).round().astype(int)
 
 print(">> process and split data", flush=True)
 train_data, test_data = split_molecules(
@@ -63,16 +71,16 @@ X_test.eliminate_zeros()
 # copy adata to train_set, test_set
 output_train = ad.AnnData(
     layers={"counts": X_train},
-    obs=adata.obs[[]],
-    var=adata.var[[]],
-    uns={"dataset_id": adata.uns["dataset_id"]}
+    obs=adata_output.obs[[]],
+    var=adata_output.var[[]],
+    uns={"dataset_id": adata_output.uns["dataset_id"]}
 )
 test_uns_keys = ["dataset_id", "dataset_name", "dataset_url", "dataset_reference", "dataset_summary", "dataset_description", "dataset_organism"]
 output_test = ad.AnnData(
     layers={"counts": X_test},
-    obs=adata.obs[[]],
-    var=adata.var[[]],
-    uns={key: adata.uns[key] for key in test_uns_keys}
+    obs=adata_output.obs[[]],
+    var=adata_output.var[[]],
+    uns={key: adata_output.uns[key] for key in test_uns_keys}
 )
 
 # add additional information for the train set

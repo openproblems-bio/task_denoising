06d5439b1c89662216bad49ddb1218570e819ca4
Jérémie Kalfon <jkobject@gmail.com>
2025-03-18 11:14:55 +0100

    Update scprint (#26)
    * improving for scprint
    
    * Update script.py
    
    * Update config.vsh.yaml
    
    * Update src/methods/scprint/config.vsh.yaml
    
    Co-authored-by: Luke Zappia <lazappi@users.noreply.github.com>
    
    * Update src/methods/scprint/config.vsh.yaml
    
    Co-authored-by: Luke Zappia <lazappi@users.noreply.github.com>
    
    * Update src/methods/scprint/config.vsh.yaml
    
    Co-authored-by: Luke Zappia <lazappi@users.noreply.github.com>
    
    * Update _viash.yaml
    
    * adding solution for large datasets
    
    * making it work
    
    * removing flag
    
    * new dataloader version
    
    * Style and lint scPRINT script
    
    * Update CHANGELOG
    
    ---------
    
    Co-authored-by: Luke Zappia <lazappi@users.noreply.github.com>
    Co-authored-by: Luke Zappia <luke@data-intuitive.com>

--- src/methods/scprint/script.py
+++ src/methods/scprint/script.py
@@ -7,7 +7,6 @@ from huggingface_hub import hf_hub_download
 from scdataloader import Preprocessor
 from scprint import scPrint
 from scprint.tasks import Denoiser
-import numpy as np
 
 ## VIASH START
 par = {
@@ -15,6 +14,9 @@ par = {
     "output": "output.h5ad",
     "model_name": "large",
     "model": None,
+    "predict_depth_mult": 5.0,
+    "max_len": 5000,
+    "batch_size": 32,
 }
 meta = {"name": "scprint"}
 ## VIASH END
@@ -26,9 +28,7 @@ input = ad.read_h5ad(par["input_train"])
 print(input)
 
 print("\n>>> Preprocessing data...", flush=True)
-adata = ad.AnnData(
-    X=input.layers["counts"]
-)
+adata = ad.AnnData(X=input.layers["counts"])
 adata.obs_names = input.obs_names
 adata.var_names = input.var_names
 if input.uns["dataset_organism"] == "homo_sapiens":
@@ -60,53 +60,61 @@ if model_checkpoint_file is None:
         repo_id="jkobject/scPRINT", filename=f"{par['model_name']}.ckpt"
     )
 print(f"Model checkpoint file: '{model_checkpoint_file}'", flush=True)
-model = scPrint.load_from_checkpoint(
-    model_checkpoint_file,
-    transformer="normal",  # Don't use this for GPUs with flashattention
-    precpt_gene_emb=None,
-)
 
 print("\n>>> Denoising data...", flush=True)
 if torch.cuda.is_available():
     print("CUDA is available, using GPU", flush=True)
     precision = "16-mixed"
     dtype = torch.float16
+    transformer = "flash"
 else:
     print("CUDA is not available, using CPU", flush=True)
     precision = "32"
     dtype = torch.float32
-n_cores_available = len(os.sched_getaffinity(0))
-print(f"Using {n_cores_available} worker cores")
+    transformer = "normal"
+
+m = torch.load(model_checkpoint_file, map_location=torch.device("cpu"))
+if "label_counts" in m["hyper_parameters"]:
+    model = scPrint.load_from_checkpoint(
+        model_checkpoint_file,
+        transformer=transformer,  # Don't use this for GPUs with flashattention
+        precpt_gene_emb=None,
+        classes=m["hyper_parameters"]["label_counts"],
+    )
+else:
+    model = scPrint.load_from_checkpoint(
+        model_checkpoint_file,
+        transformer=transformer,  # Don't use this for GPUs with flashattention
+        precpt_gene_emb=None,
+    )
+del m
+
+n_cores = min(len(os.sched_getaffinity(0)), 24)
+print(f"Using {n_cores} worker cores")
 denoiser = Denoiser(
-    num_workers=n_cores_available,
+    num_workers=n_cores,
     precision=precision,
-    max_cells=adata.n_obs,
+    max_cells=adata.n_obs + 1000,
+    max_len=par["max_len"],
+    batch_size=par["batch_size"],
+    predict_depth_mult=par["predict_depth_mult"],
+    downsample=False,
     doplot=False,
     dtype=dtype,
+    how="most var",
 )
-_, idxs, genes, expr_pred = denoiser(model, adata)
-print(f"Predicted expression dimensions: {expr_pred.shape}")
+_, idxs, output = denoiser(model, adata)
+print(f"Predicted expression dimensions: {output.shape}")
 
-print("\n>>> Applying denoising...", flush=True)
-adata.X = adata.X.tolil()
-idxs = idxs if idxs is not None else range(adata.shape[0])
-for i, idx in enumerate(idxs):
-    adata.X[idx, adata.var.index.get_indexer(genes)] = expr_pred[i]
-adata.X = adata.X.tocsr()
-print(adata)
 
-print("\n>>> Storing output...", flush=True)
-output = ad.AnnData(
-    layers={
-        "denoised": adata.X[:, adata.var.index.get_indexer(input.var_names)],
-    },
-    obs=input.obs[[]],
-    var=input.var[[]],
-    uns={
-        "dataset_id": input.uns["dataset_id"],
-        "method_id": meta["name"],
-    },
-)
+output.layers["denoised"] = output.layers["scprint_mu"]
+output.uns["method_id"] = meta["name"]
+output.uns["dataset_id"] = input.uns["dataset_id"]
+output.obs = input.obs[[]]
+
+print("\n>>> subsetting output to original genes...", flush=True)
+output = output[:, output.var.index.get_indexer(input.var_names)]
+
 print(output)
 
 print("\n>>> Writing output AnnData to file...", flush=True)
